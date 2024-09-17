570965ea85162948dcc14f4eeb9b6f311cb2026f
tomeytran <tmtri310251@gmail.com>
2024-09-17 11:08:32 +0700

    for loop

---
+++ main.js
@@ -0,0 +1,17 @@
+// BAI 1
+// let text = "";
+// let j;
+// for (j = 0; j < 5; j++) {
+// 	text += "The number is " + j + "<br>";
+// }
+// document.getElementById("demo").innerHTML = text;
+
+// BAI 2
+let num = prompt("Enter your number: ");
+let total = 0;
+let i = 1;
+for (; i <= num; i++) {
+	total = total + i;
+	alert(total);
+}
+8;
